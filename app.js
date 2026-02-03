1bc065650f06bf8d28a857cfb063414d61558303
Elvin Mendez <[elvinmanuel005@gmail.com]>
2026-02-02 23:08:32 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Ruta de prueba
+app.get("/", (req, res) => {
+  res.json({
+    ok: true,
+    message: "API funcionando correctamente"
+  });
+});
+
+module.exports = app; // 👈 ESTO ES CLAVE
