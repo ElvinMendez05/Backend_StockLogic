a303e2bc84698143a357ef649fedd6f5ceeb4fc6
Reymon Ruiz Reyes <20240081@itla.edu.do>
2026-03-03 22:05:33 -0400

    feat: Implemetar sistema de autorización por rol

--- models/seedLoaders/defaultRoles.js
+++ models/seedLoaders/defaultRoles.js
@@ -4,9 +4,9 @@ const seedDefaultRoles = async (RolesModel) => {
     const count = await RolesModel.count();
     if (count === 0) {
         await RolesModel.bulkCreate([
-            { name: 'SUPER_ADMIN', description: 'Dueño de la empresa' },
-            { name: 'ADMIN', description: 'Administrador' },
-            { name: 'EMPLOYEE', description: 'Empleado' }
+            { name: 'SUPER_ADMIN', description: 'Company owner', code: 'SUPER_ADMIN' },
+            { name: 'ADMIN', description: 'Administrator', code: 'ADMIN' },
+            { name: 'EMPLOYEE', description: 'Employee', code: 'EMPLOYEE' }
         ]);
         console.log("Default roles created.");
     }
